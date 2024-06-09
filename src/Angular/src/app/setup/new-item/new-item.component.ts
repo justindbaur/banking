b52b823992905607530b15eb24b96688eee9d1f3
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-06-09 13:21:49 -0400

    Work On Forms

--- src/Angular/src/app/setup/new-item/new-item.component.ts
+++ src/Angular/src/app/setup/new-item/new-item.component.ts
@@ -1,11 +1,13 @@
-import { AsyncPipe, CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, KeyValuePipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject, Input } from '@angular/core';
 import {
   AbstractControl,
   FormBuilder,
   FormControl,
+  FormGroup,
   FormsModule,
+  ReactiveFormsModule,
 } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, map, switchMap } from 'rxjs';
@@ -13,98 +15,53 @@ import { BASE_URL } from '../../app.module';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 
-type CustomFormEntry = CustomFormInput | CustomFormGroup;
-type CustomFormInput = { type: 'input'; id: string; description: string };
-type CustomFormGroup = {
-  type: 'group';
-  id: string;
-  entries: CustomFormEntry[];
+type FormInput = {
+  type: 'text' | 'textarea' | 'number';
+  label: string;
+  hideInput: boolean;
+  description?: string;
+  order?: number;
 };
 
-export const convertSchemaToForm = (
-  schema: unknown,
-  parent: string
-): CustomFormEntry => {
-  if (schema == null) {
-    throw new Error('A schema object must be non-null');
-  }
-
-  if (typeof schema !== 'object') {
-    throw new Error('A schema object should be an object');
-  }
-
-  if (!('type' in schema) || typeof schema.type !== 'string') {
-    throw new Error('A schema must have a type property of type string');
-  }
-
-  switch (schema.type) {
-    case 'string':
-      return {
-        type: 'input',
-        id: parent,
-        description: (schema as any).description,
-      };
-    case 'object': {
-      // Validate
-      if (
-        !('properties' in schema) ||
-        typeof schema.properties !== 'object' ||
-        schema.properties == null ||
-        Array.isArray(schema.properties)
-      ) {
-        throw new Error(
-          'A type object must have a properties property that is dict like.'
-        );
-      }
-
-      // Get a control for every property
-      const controls = Object.entries(schema.properties).map(
-        ([name, subSchema]) => {
-          return convertSchemaToForm(subSchema, name);
-        }
-      );
-
-      return { type: 'group', id: parent, entries: controls };
-    }
-    default:
-      throw new Error(`Unsupported schema type: ${schema.type}`);
-  }
+type FormSelect = {
+  type: 'select';
+  label: string;
+  description?: string;
+  values: Record<string, string>;
+  order?: number;
 };
+
+type AnyFormInput = FormInput | FormSelect;
 
 export type StartToken = {
-  requirementsSchema: unknown;
+  requirementsSchema: { inputs: Record<string, AnyFormInput> };
   state: string | null;
 };
 
 @Component({
-  selector: 'app-form-element',
   standalone: true,
-  imports: [CommonModule, MatInputModule, FormsModule],
-  templateUrl: './form-element.html',
-})
-export class FormElement {
-  @Input({ required: true, alias: 'entry' })
-  entry: CustomFormEntry = null!;
-}
-
-@Component({
-  standalone: true,
-  template: `<form #dynForm (ngSubmit)="submit(dynForm)">
-    <ng-container *ngIf="start$ | async as start">
-      <app-form-element [entry]="start.form"></app-form-element>
-      <input type="hidden" value="{{ start.state }}" />
-      <button mat-raised-button color="primary" type="submit">Next</button>
-    </ng-container>
-  </form>`,
-  imports: [AsyncPipe, CommonModule, FormElement, MatButtonModule],
+  imports: [
+    AsyncPipe,
+    KeyValuePipe,
+    CommonModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
+  templateUrl: './new-item.component.html',
 })
 export class NewItemComponent {
-  start$: Observable<{ state: string | null; form: CustomFormEntry }>;
+  start$: Observable<{
+    state: string | null;
+    inputs: Record<string, AnyFormInput>;
+    formGroup: FormGroup;
+  }>;
 
   constructor(
     route: ActivatedRoute,
     private readonly httpClient: HttpClient,
-    @Inject(BASE_URL) private readonly baseUrl: string
+    @Inject(BASE_URL) private readonly baseUrl: string,
+    private readonly formBuilder: FormBuilder
   ) {
     const sourceId = route.paramMap.pipe(map((p) => p.get('id')!));
 
@@ -115,16 +72,30 @@ export class NewItemComponent {
         );
       }),
       map((startToken) => {
-        // Parse requirements
+        console.log(startToken.requirementsSchema.inputs);
+        const controls: Record<string, FormControl> = {};
+        const inputs = Object.entries(
+          startToken.requirementsSchema.inputs
+        ).sort(
+          ([aKey, aValue], [bKey, bValue]) =>
+            (aValue.order ?? 0) - (bValue.order ?? 0)
+        );
+
+        console.log(inputs);
+        for (const key of Object.keys(Object.fromEntries(inputs))) {
+          controls[key] = this.formBuilder.control('', { nonNullable: true });
+        }
+
         return {
           state: startToken.state,
-          form: convertSchemaToForm(startToken.requirementsSchema, '__root'),
+          inputs: Object.fromEntries(inputs),
+          formGroup: this.formBuilder.group(controls),
         };
       })
     );
   }
 
-  submit(s: any) {
-    console.log('form', s);
+  submit(form: FormGroup, state: string | null) {
+    console.log('form', form);
   }
 }
